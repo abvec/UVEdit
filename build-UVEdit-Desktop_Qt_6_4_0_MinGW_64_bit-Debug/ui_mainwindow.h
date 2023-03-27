f8c17dcbf09c86eb2b12a3ca3eaac01407cf19c1
abvec <111785204+abvec@users.noreply.github.com>
2023-03-27 22:12:16 +0100

    Opengl setup

--- build-UVEdit-Desktop_Qt_6_4_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-UVEdit-Desktop_Qt_6_4_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -10,10 +10,17 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtOpenGLWidgets/QOpenGLWidget>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QScrollBar>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTreeWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,6 +29,16 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout_3;
+    QTreeWidget *treeWidget;
+    QOpenGLWidget *openGLWidget_2;
+    QGridLayout *gridLayout;
+    QOpenGLWidget *openGLWidget;
+    QScrollBar *horizontalScrollBar;
+    QScrollBar *verticalScrollBar;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +46,77 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(538, 415);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        horizontalLayout_2 = new QHBoxLayout(centralwidget);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName("horizontalLayout");
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName("verticalLayout");
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName("horizontalLayout_3");
+        treeWidget = new QTreeWidget(centralwidget);
+        QTreeWidgetItem *__qtreewidgetitem = new QTreeWidgetItem();
+        __qtreewidgetitem->setText(0, QString::fromUtf8("1"));
+        treeWidget->setHeaderItem(__qtreewidgetitem);
+        treeWidget->setObjectName("treeWidget");
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(1);
+        sizePolicy.setVerticalStretch(1);
+        sizePolicy.setHeightForWidth(treeWidget->sizePolicy().hasHeightForWidth());
+        treeWidget->setSizePolicy(sizePolicy);
+
+        horizontalLayout_3->addWidget(treeWidget);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        openGLWidget_2 = new QOpenGLWidget(centralwidget);
+        openGLWidget_2->setObjectName("openGLWidget_2");
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(1);
+        sizePolicy1.setVerticalStretch(1);
+        sizePolicy1.setHeightForWidth(openGLWidget_2->sizePolicy().hasHeightForWidth());
+        openGLWidget_2->setSizePolicy(sizePolicy1);
+
+        verticalLayout->addWidget(openGLWidget_2);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        gridLayout = new QGridLayout();
+        gridLayout->setObjectName("gridLayout");
+        openGLWidget = new QOpenGLWidget(centralwidget);
+        openGLWidget->setObjectName("openGLWidget");
+        sizePolicy1.setHeightForWidth(openGLWidget->sizePolicy().hasHeightForWidth());
+        openGLWidget->setSizePolicy(sizePolicy1);
+
+        gridLayout->addWidget(openGLWidget, 0, 1, 1, 1);
+
+        horizontalScrollBar = new QScrollBar(centralwidget);
+        horizontalScrollBar->setObjectName("horizontalScrollBar");
+        horizontalScrollBar->setOrientation(Qt::Horizontal);
+
+        gridLayout->addWidget(horizontalScrollBar, 1, 1, 1, 1);
+
+        verticalScrollBar = new QScrollBar(centralwidget);
+        verticalScrollBar->setObjectName("verticalScrollBar");
+        verticalScrollBar->setOrientation(Qt::Vertical);
+
+        gridLayout->addWidget(verticalScrollBar, 0, 0, 1, 1);
+
+
+        horizontalLayout->addLayout(gridLayout);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 538, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
